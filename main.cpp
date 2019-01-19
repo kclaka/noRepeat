a45501c33ab0fbce4e884f7453fb2de84d38db80
Kenny Claka <kennyigbechi@gmail.com>
2019-01-19 00:41:20 -0800

    r

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    string previous = "";
+    int numWords = 0;
+    string current;
+
+    while(cin>>current){
+        ++numWords;
+        if(previous == current)
+            cout<<"Repeated Word found: "<< current<<" at number "<<numWords<<endl;
+        previous = current;
+
+    }
+
+
+    return 0;
+}
